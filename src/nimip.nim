2358ee8e6ceaf974413b4c8d215016069eec7b7a
HitBlast <hitblastlive@gmail.com>
2023-06-29 01:58:11 +0600

    cope with main file changes

--- src/nimip.nim
+++ src/nimip.nim
@@ -1,27 +1,4 @@
-#[
-    MIT License
-
-    Copyright (c) 2023 HitBlast
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-]#
-
+# # SPDX-License-Identifier: MIT
 
 
 # Imports.
@@ -43,7 +20,6 @@ proc run*(): Future[void] {.async.} =
             help("Asynchronously fetch information on IP addresses.")
             arg("address", help="The IP address to query for.")
         ip: IPRef
-        is_error = false
 
     # Attempt to parse the command-line input and handle possible exceptions.
     try:
@@ -55,22 +31,20 @@ proc run*(): Future[void] {.async.} =
     except ShortCircuit as err:
         if err.flag == "argparse_help":
             echo err.help
-            is_error = true
+            quit(1)
 
     except UsageError:
         stderr.writeLine getCurrentExceptionMsg()
-        is_error = true
+        quit(1)
 
     except OSError:
         echo "Make sure you have a stable internet connection."
-        is_error = true
+        quit(1)
 
     except IPResponseError:
-        echo "Could not query the given IP address."
-        is_error = true
-
-    if is_error:
+        echo "Couldn\'t query the given IP address."
         quit(1)
+        
 
     # Initialize an instance of illwave and run the TUI if the code above succeeds.
     # This includes a cursor-less window, so an exit procedure is also required.
@@ -98,16 +72,22 @@ proc run*(): Future[void] {.async.} =
     tb.drawHorizLine(2, primaryLength - 2, 2, doubleStyle=true)
 
     # Display IP information onto terminal.
-    tb.write(2, 4,  "Address     : ", fgGreen, ip.address, fgWhite)
-    tb.write(2, 5,  "Timezone    : ", ip.timezone, fmt" | Offset: {ip.offset}")
-    tb.write(2, 6,  "Location    : ", addressStr)
-    tb.write(2, 9,  "Provider    : ", ip.isp)
-    tb.write(16, 10, fmt"[{ip.orgAs}]")
-    tb.write(2, 12, "Zip Code    : ", ip.zip)
+    tb.write(4, 4,  "Address     : ", fgGreen, ip.address, fgWhite)
+    tb.write(4, 5,  "Timezone    : ", ip.timezone, fmt" | Offset: {ip.offset}")
+    tb.write(4, 6,  "Location    : ", addressStr)
 
-    tb.drawHorizLine(2, primaryLength - 2, 13)
-    tb.write(2, 15, "Latitude    : ", fgCyan, fmt"{ip.latitude}", fgWhite)
-    tb.write(2, 16, "Longitude   : ", fgCyan, fmt"{ip.longitude}", fgWhite)
+    tb.drawVertLine(0, 17, 1, doubleStyle=true)
+    tb.write(4, 9,  "Provider    : ", ip.isp)
+    tb.write(18, 10, fmt"[{ip.orgAs}]")
+    tb.write(4, 12, "Zip Code    : ", ip.zip)
+
+    tb.drawHorizLine(1, primaryLength - 2, 13)
+    tb.write(4, 15, "Latitude    : ", fgCyan, fmt"{ip.latitude}", fgWhite)
+    tb.write(4, 16, "Longitude   : ", fgCyan, fmt"{ip.longitude}", fgWhite)
+
+    tb.drawHorizLine(1, primaryLength - 2, 17)
+    tb.write(primaryLength, 13, fgGreen, fmt"{ip.remainingRequests}", fgWhite, " remaining requests.")
+    tb.write(primaryLength, 17, fgGreen, fmt"{ip.timeUntilReset}s", fgWhite, " until cache reset.")
 
 
     # Finally, display the entire thing.
